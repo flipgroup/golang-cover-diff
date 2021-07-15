14a6792830b77bf7bb5eb9928c8fe9bd59c1aac7
Peter Mescalchin <peter@flip.com.au>
2021-07-15 12:38:07 +1000

    A few package line metric tests

--- report_test.go
+++ report_test.go
@@ -28,7 +28,55 @@ func TestReportParser(t *testing.T) {
 		r := strings.NewReader("mode: set")
 		result, err := parseCoverProfile(r)
 
+		assert.Equal(t, "set", result.Mode)
+		assert.NoError(t, err)
+	})
+
+	t.Run("single coverage line", func(t *testing.T) {
+		r := strings.NewReader(`mode: set
+github.com/flipgroup/module/package/file.go:22.39,24.2 1 1
+`)
+		result, err := parseCoverProfile(r)
+
 		assert.NotNil(t, result)
 		assert.NoError(t, err)
+	})
+
+	t.Run("malformed coverage line", func(t *testing.T) {
+		r := strings.NewReader(`mode: set
+github.com/flipgroup/module/package/file.go:22.39,24.2 1 1
+github.com/flipgroup/module/package/file.go:22.39,24.2 1 BLURG
+`)
+		result, err := parseCoverProfile(r)
+
+		assert.Nil(t, result)
+		assert.Error(t, err)
+	})
+
+	t.Run("valid coverage lines", func(t *testing.T) {
+		r := strings.NewReader(`mode: set
+github.com/flipgroup/module/package/file.go:22.39,24.2 5 1
+github.com/flipgroup/module/package/file.go:44.39,24.2 3 0
+github.com/flipgroup/module/package/file.go:66.39,24.2 2 1
+github.com/flipgroup/module/package/file.go:88.39,24.2 1 0
+github.com/flipgroup/module/another/file.go:22.39,24.2 20 1
+github.com/flipgroup/module/another/file.go:44.39,24.2 40 1
+github.com/flipgroup/module/another/file.go:66.39,24.2 60 0
+github.com/flipgroup/module/another/file.go:88.39,24.2 80 0
+`)
+		result, err := parseCoverProfile(r)
+		assert.NoError(t, err)
+
+		// verify cover and individual package metrics
+		assert.Equal(t, 211, result.Total)
+		assert.Equal(t, 67, result.Covered)
+
+		pkg01 := result.Packages["github.com/flipgroup/module/package"]
+		assert.Equal(t, 11, pkg01.Total)
+		assert.Equal(t, 7, pkg01.Covered)
+
+		pkg02 := result.Packages["github.com/flipgroup/module/another"]
+		assert.Equal(t, 200, pkg02.Total)
+		assert.Equal(t, 60, pkg02.Covered)
 	})
 }
